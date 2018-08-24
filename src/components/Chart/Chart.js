5b7b84bb8ff74b1ac50352415bbeed98b0a148cf
Nikita Neganov <nneganov001@cee.ema.ad.pwcinternal.com>
2018-08-24 17:37:33 +0500

    ddd

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,158 +3,22 @@ import { connect } from 'react-redux';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import products, { options } from '../../data';
+import { getRandomColor } from '../../utils.js';
 import * as actions from '../../redux/actions';
 // import options from './options';
 
 class Chart extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayData: this.getDataByType(this.props.data, this.props.displayType),
-    };
-  }
-
-  getDataByType(data, type) {
-    const seriesData = [];
-    if (type === 'Goods') {
-      data.forEach((obj) => {
-        seriesData.push({
-          name: obj.name,
-          color: this.getRandomColor(),
-          data: [[obj.feature1, obj.feature2]],
-        });
-      });
-    } else if (type === 'Groups of Goods') {
-      const seriesMore150 = [];
-      const seriesLess100 = [];
-      const seriesOther = [];
-      data.forEach((el) => {
-        el.forEach((obj) => {
-          if (obj.feature1 > 150) {
-            seriesMore150.push([obj.feature1, obj.feature2]);
-          } else if (obj.feature1 < 100) {
-            seriesLess100.push([obj.feature1, obj.feature2]);
-          } else {
-            seriesOther.push([obj.feature1, obj.feature2]);
-          }
-        });
-      });
-      seriesData.push(
-
-        {
-          name: 'Feature1 > 150',
-          color: this.getRandomColor(),
-          data: seriesMore150,
-        }, {
-          name: 'Feature1 < 100',
-          color: this.getRandomColor(),
-          data: seriesLess100,
-        }, {
-          name: 'Other',
-          color: this.getRandomColor(),
-          data: seriesOther,
-        },
-
-      );
-    }
-    return seriesData;
-  }
-
-  getDataByProps(year, displayType) {
-    const reduceData = data => data.reduce((previousValue, newValue) => {
-      const existingData = previousValue[newValue.year];
-      if (existingData) {
-        return {
-          ...previousValue,
-          [newValue.year]: [...existingData, newValue],
-        };
-      }
-
-      return {
-        ...previousValue,
-        [newValue.year]: [newValue],
-      };
-    }, {});
-    const productData = reduceData(products);
-    return this.getDataByDisplayType(productData[year], displayType);
-  }
-
-  getDataByDisplayType(data, displayType) {
-    const seriesData = [];
-    if (displayType === 'Goods') {
-      data.forEach((obj) => {
-        seriesData.push(
-          {
-            name: obj.name,
-            color: this.getRandomColor(),
-            data: [[obj.feature1, obj.feature2]],
-          },
-        );
-      });
-    } else if (displayType === 'Groups of Goods') {
-      const seriesMore150 = [];
-      const seriesLess100 = [];
-      const seriesOther = [];
-      data.forEach((obj) => {
-        if (obj.feature1 > 150) {
-          seriesMore150.push(
-            [obj.feature1, obj.feature2],
-          );
-        } else if (obj.feature1 < 100) {
-          seriesLess100.push(
-            [obj.feature1, obj.feature2],
-          );
-        } else {
-          seriesOther.push(
-            [obj.feature1, obj.feature2],
-          );
-        }
-      });
-      seriesData.push(
-
-        {
-          name: 'Feature1 > 150',
-          color: this.getRandomColor(),
-          data: seriesMore150,
-        }, {
-          name: 'Feature1 < 100',
-          color: this.getRandomColor(),
-          data: seriesLess100,
-        }, {
-          name: 'Other',
-          color: this.getRandomColor(),
-          data: seriesOther,
-        },
-
-      );
-    }
-    console.log('SeriesData inside getdataByDisplayType', seriesData);
-    return seriesData;
-  }
-
-  getRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
 
   yearChangedHandler = (event) => {
     const year = Number(event.target.value);
     this.props.onSetYear(year);
-    this.setState({
-      displayData: this.getDataByProps(year, this.props.displayType),
-    });
+    
   }
 
   displayTypeChangedHandler = (event) => {
     const displayType = event.target.value;
     this.props.onSetType(displayType);
-    this.setState({
-      displayData: this.getDataByType(this.props.data, displayType),
-    });
+    
   }
 
   render() {
@@ -173,7 +37,7 @@ class Chart extends Component {
           highcharts={Highcharts}
           options={{
             ...options,
-            series: this.state.displayData,
+            series: this.props.data,
           }}
         />
       </div>
@@ -182,18 +46,24 @@ class Chart extends Component {
 }
 
 const selectDataByGroups = data => data.reduce((prevValue, newValue) => {
-  if (newValue.feature1 > 150) {
-    prevValue[2].push(newValue);
-  } else if (newValue.feature1 < 100) {
-    prevValue[0].push(newValue);
+  console.log(newValue.data)
+  if (newValue.data[0] > 150) {
+    prevValue[2].push(newValue.data);
+  } else if (newValue.data[0] < 100) {
+    prevValue[0].push(newValue.data);
   } else {
-    prevValue[1].push(newValue);
+    prevValue[1].push(newValue.data);
   }
   return prevValue;
-}, [[], [], []]);
+}, [[], [], []]).map((v, i) => ({
+  name: 'Group ' + (i + 1),
+  color: getRandomColor(),
+  data: v,
+}));
 
 const mapStateToProps = (state) => {
   const { displayType, data, year } = state.data;
+  console.log(selectDataByGroups(data[year]));
   return {
     year,
     displayType,
--- src/redux/dataReducer.js
+++ src/redux/dataReducer.js
@@ -1,18 +1,24 @@
 import * as constants from './constants';
 import products from '../data';
+import { getRandomColor } from '../utils';
 
 const reduceData = data => data.reduce((previousValue, newValue) => {
+  const newData = {
+    name: newValue.name,
+    color: getRandomColor(),
+    data: [[newValue.feature1, newValue.feature2]],
+  };
   const existingData = previousValue[newValue.year];
   if (existingData) {
     return {
       ...previousValue,
-      [newValue.year]: [...existingData, newValue],
+      [newValue.year]: [...existingData, newData],
     };
   }
 
   return {
     ...previousValue,
-    [newValue.year]: [newValue],
+    [newValue.year]: [newData],
   };
 }, {});
 
